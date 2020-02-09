1b0eeec462edee1f6ad2232daca2093f6e8bbe67
bitturids <bitturids@gmail.com>
2020-02-09 22:42:27 +0800

    first commit with basic setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+app.listen(3000,()=>{console.log("up and running!")});
+
+
+
